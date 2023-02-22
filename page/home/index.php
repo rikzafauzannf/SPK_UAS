432d2be057cd31e976cc5f95ebfeceb24afb4dd8
Rikza Fauzan <67640598+rikzafauzannf@users.noreply.github.com>
2023-02-22 12:19:01 +0700

    update

--- page/home/index.php
+++ page/home/index.php
@@ -1,13 +1,13 @@
 <section class="hero is-medium is-bold" style="width:100%;height:100vh;background-image:linear-gradient(to left bottom,blue,purple);color:white;">
     <div class="hero-body">
         <div class="container">
-            <h1 class=" animate__animated animate__zoomIn">
+            <h1 class="title animate__animated animate__zoomIn" style="color:white;">
                 SPK TEMPAT LES
             </h1>
-            <h2 class=" animate__animated animate__slideInUp">
+            <h2 class="subtitle animate__animated animate__slideInUp" style="color:white;">
                 Project UAS Kelompok
             </h2>
-            <ol class="animate__animated animate__slideInUp">
+            <ol class="subtitle animate__animated animate__slideInUp" style="color:white;">
                 <li>Rikza Fauzan Nurfadilah</li>
                 <li>Yusri Azra Lazwardi</li>
                 <li>Fadhel Muhammad Apriansyah</li>
